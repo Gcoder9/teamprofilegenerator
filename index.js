cc7d7480f8c4dc90c3d1f83e25e382abe5d0cc4f
Gcoder9 <104526581+Gcoder9@users.noreply.github.com>
2022-06-17 10:39:46 -0400

    varibles

---
+++ index.js
@@ -0,0 +1,11 @@
+//fs for writing file
+const fs = require('fs');
+
+//inquirer for prompting user for their response
+const inquirer = require('inquirer');
+
+// importing constructor functions
+const Employee = require('./Emp/Employee');
+const Engineer = require('./Emp/Engineer');
+const Intern = require('./Emp/Intern');
+const Manager = require('./Emp/Manager');
